875a73933364b838e32759269261b78b439da65c
Irene Choi <irenelove112@naver.com>
2024-10-07 14:21:24 +0900

    lab03_colorTV

---
+++ src/lab03/ColorTVEx.java
@@ -0,0 +1,8 @@
+package lab03;
+
+public class ColorTVEx {
+    public static void main(String[] args) {
+        ColorTV myTv = new ColorTV(65, 65536);  // ColorTV 객체 생성
+        myTv.printProperty();  // TV 정보 출력
+    }
+}
